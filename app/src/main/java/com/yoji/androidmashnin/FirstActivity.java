b9dd59538ef8273c1f49fae2c24cab5672262471
Mashnin Egor <y0g@mail.ru>
2020-04-21 12:25:09 +1000

    Initial commit

---
+++ app/src/main/java/com/yoji/androidmashnin/FirstActivity.java
@@ -0,0 +1,14 @@
+package com.yoji.androidmashnin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
